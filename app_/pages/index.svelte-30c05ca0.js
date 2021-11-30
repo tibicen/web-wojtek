281a545563f0d876af94d444ac1f0c35cb35885a
Dawid Huczynski <dawid.huczynski@pm.me>
2021-11-30 22:48:55 +0100

    update navbar and section ids

---
+++ app_/pages/index.svelte-30c05ca0.js
@@ -0,0 +1,109 @@
+import{S as Q,i as X,s as ee,e as g,t as b,k as $,c as j,a as h,g as E,d as c,n as S,b as n,f as _,H as z,h as te,J as R,K as x,L as se,M as pe,I as W,N as de,O as ue,A as me,j as M,m as K,o as D,x as A,u as I,v as Z,P as we,Q as le}from"../chunks/vendor-cf514551.js";import{N as fe}from"../chunks/NavWH-b35bd0da.js";import{H as ye,S as ae}from"../chunks/Section-36f4ef61.js";import{_ as he}from"../chunks/preload-helper-56798db9.js";function ce(m,e,a){const t=m.slice();return t[5]=e[a],t}function ze(m){let e,a=m[5].date+"",t,i,l,w,u=m[5].title+"",k,d,y,o=m[5].description+"",r,f;return{c(){e=g("time"),t=b(a),i=$(),l=g("div"),w=g("p"),k=b(u),d=$(),y=g("p"),r=b(o),f=$(),this.h()},l(s){e=j(s,"TIME",{});var p=h(e);t=E(p,a),p.forEach(c),i=S(s),l=j(s,"DIV",{class:!0});var v=h(l);w=j(v,"P",{class:!0});var O=h(w);k=E(O,u),O.forEach(c),d=S(v),y=j(v,"P",{class:!0});var T=h(y);r=E(T,o),T.forEach(c),f=S(v),v.forEach(c),this.h()},h(){n(w,"class","title"),n(y,"class","description"),n(l,"class","svelte-1w5uapt")},m(s,p){_(s,e,p),z(e,t),_(s,i,p),_(s,l,p),z(l,w),z(w,k),z(l,d),z(l,y),z(y,r),z(l,f)},p(s,p){p&2&&a!==(a=s[5].date+"")&&te(t,a),p&2&&u!==(u=s[5].title+"")&&te(k,u),p&2&&o!==(o=s[5].description+"")&&te(r,o)},d(s){s&&c(e),s&&c(i),s&&c(l)}}}function ke(m){let e,a,t,i,l,w,u,k,d,y=m[1],o=[];for(let r=0;r<y.length;r+=1)o[r]=ze(ce(m,y,r));return{c(){e=g("input"),a=$(),t=g("label"),i=R("svg"),l=R("path"),w=$(),u=g("div");for(let r=0;r<o.length;r+=1)o[r].c();this.h()},l(r){e=j(r,"INPUT",{id:!0,type:!0,class:!0}),a=S(r),t=j(r,"LABEL",{for:!0,class:!0});var f=h(t);i=x(f,"svg",{id:!0,fill:!0,"stroke-linecap":!0,"stroke-linejoin":!0,"stroke-width":!0,viewBox:!0,stroke:!0,class:!0});var s=h(i);l=x(s,"path",{d:!0}),h(l).forEach(c),s.forEach(c),f.forEach(c),w=S(r),u=j(r,"DIV",{id:!0,class:!0});var p=h(u);for(let v=0;v<o.length;v+=1)o[v].l(p);p.forEach(c),this.h()},h(){n(e,"id","accordion"),n(e,"type","checkbox"),n(e,"class","svelte-1w5uapt"),n(l,"d","M2 7  12 16 22 7"),n(i,"id","arrow"),n(i,"fill","none"),n(i,"stroke-linecap","round"),n(i,"stroke-linejoin","round"),n(i,"stroke-width","3"),n(i,"viewBox","0 0 24 24"),n(i,"stroke","currentColor"),n(i,"class","svelte-1w5uapt"),se(i,"rotate",m[0]),n(t,"for","accordion"),n(t,"class","svelte-1w5uapt"),n(u,"id","cv"),n(u,"class","svelte-1w5uapt")},m(r,f){_(r,e,f),_(r,a,f),_(r,t,f),z(t,i),z(i,l),_(r,w,f),_(r,u,f);for(let s=0;s<o.length;s+=1)o[s].m(u,null);k||(d=pe(t,"click",m[2]),k=!0)},p(r,[f]){if(f&1&&se(i,"rotate",r[0]),f&2){y=r[1];let s;for(s=0;s<y.length;s+=1){const p=ce(r,y,s);o[s]?o[s].p(p,f):(o[s]=ze(p),o[s].c(),o[s].m(u,null))}for(;s<o.length;s+=1)o[s].d(1);o.length=y.length}},i:W,o:W,d(r){r&&c(e),r&&c(a),r&&c(t),r&&c(w),r&&c(u),de(o,r),k=!1,d()}}}function ge(m,e,a){let t=!1,i=`2014-obecnie
+W\u0142a\u015Bciciel
+Dzia\u0142alno\u015B\u0107 gospodarcza
+
+2016-obecnie
+Cz\u0142onek Rady Nadzorcej Sp\xF3\u0142ki.
+Zak\u0142ad Komunalny w Opolu sp. z o.o.
+
+2015-2016
+Zast\u0119pca przewodnicz\u0105cego Rady Nadzorcej Sp\xF3\u0142ki.
+Projekt K\u0119pno sp. z o.o.
+
+2015
+Dyrektor ds. Automatyzacji Proces\xF3w Ciep\u0142owniczych
+ENERGA Kogeneracja sp. z o.o.
+Koordynacja automatyzacji w\u0119z\u0142\xF3w ciep\u0142owniczych i modelowania sieci w sp\xF3\u0142ce Ciep\u0142o Kaliskie. Projekty na ok. 4,5 mln z\u0142  
+
+2002-2014
+Burmistrz Miasta Brzeg
+Urz\u0105d Miasta Brzeg 
+(trzy kadencje) \u2013 jednoosobowe zarz\u0105dzanie corocznym bud\u017Cetem ponad 100 mln. z\u0142otych, pozytywne oceny dzia\u0142alno\u015Bci finansowej przez organy kontroluj\u0105ce, wiele nagr\xF3d z powodu dobrego gospodarowania miastem pocz\u0105wszy od nagr\xF3d za przestrze\u0144 publiczn\u0105, poprzez nagrody za sprawy spo\u0142eczne do nagr\xF3d za najlepsze wykorzystanie \u201E\u015Brodk\xF3w europejskich\u201D, odd\u0142u\u017Cenie miasta, skuteczne zarz\u0105dzanie w zakresie kryzysowym (powodzie).
+\u2013 Bezpo\u015Brednie zarz\u0105dzanie oko\u0142o 150 osobami, a poprzez jednostki organizacyjne, sp\xF3\u0142ki, itp ponad 700 osobami.
+\u2013 Dziesi\u0105tki zrealizowanych projekt\xF3w inwestycyjnych o warto\u015Bci od kilkuset tysi\u0119cy po kilkadziesi\u0105t milion\xF3w z\u0142otych.
+\u2013 Projekt Oczyszczanie \u015Bciek\xF3w w Brzegu \u2013 projekt ponad 30 mln EURO
+\u2013 Projekt System gospodarki odpadami \u015Al\u0119za \u2013 O\u0142awa projekt ponad 30 mln. EURO;
+\u2013 Budowa Centrum Sportowo-Rekreacyjnego w Brzegu \u2013 ponad 30 mln. z\u0142.
+\u2013 Termomodernizacja budynk\xF3w u\u017Cyteczno\u015Bci publicznej \u2013 ok. 15 mln. z\u0142.
+\u2013 Wiele projekt\xF3w infrastrukturalnych do 5 mln. z\u0142. 
+
+2011-2015
+Przewodnicz\u0105cy Rady Nadzorcej Sp\xF3\u0142ki.
+Zak\u0142ad Gospodarki Odpadami w Ga\u0107 sp. z o.o.
+
+2011-2014
+Przewodnicz\u0105cy Rady Nadzorcej Sp\xF3\u0142ki.
+U.W-K HYDROLEW Lewin Brzeski sp. z o.o.
+
+2007-2014
+Przewodnicz\u0105cy Zarz\u0105du Zwi\u0105zku
+Ekologiczny Zwi\u0105zek Gospodarki Odpadami Komunalnymi Mi\u0119dzygminny \u201EEKOGOK\u201D
+Realizacja wielu projekt\xF3w dotycz\u0105cych przetwarzania odpad\xF3w, w tym projekt ponad 30 mln. EURO dotycz\u0105cy \u201ESystemu gospodarki odpadami \u015Al\u0119za \u2013 O\u0142awa\u201D
+
+2002-2014
+Nadz\xF3r w\u0142a\u015Bcicielski sp\xF3\u0142ki
+Brzeskie Przedsi\u0119biorstwo Energetyki Cieplnej sp. z o.o.
+Brzeskie Towarzystwo Budownictwa Spo\u0142ecznego sp. z o.o.
+Miejski Zarz\u0105d Mienia Komunalnego w Brzegu sp. z o.o.
+Zak\u0142ad Higieny Komunalnej w Brzegu sp. z o.o.
+Przedsi\u0119biorstwo Wodoci\u0105g\xF3w i Kanalizacji w Brzegu sp. z o.o.
+
+2004-2007
+Measure Authorizing Officer
+Fundusz Sp\xF3jno\u015Bci \u201EOczyszczanie \u015Bciek\xF3w w Brzegu\u201D
+projekt szacowany na ponad 30 mln Euro
+
+2003-2008
+Cz\u0142onek Rady Programowej
+Radio Opole S.A.
+
+2002-2007
+Przewodnicz\u0105cy Zgromadzenia
+Ekologiczny Zwi\u0105zek Gospodarki Odpadami Komunalnymi Mi\u0119dzygminny \u201EEKOGOK\u201D
+Realizacja wielu projekt\xF3w dotycz\u0105cych przetwarzania odpad\xF3w, w tym projekt ponad 30 mln. EURO dotycz\u0105cy \u201ESystemu gospodarki odpadami \u015Al\u0119za \u2013 O\u0142awa\u201D
+
+2000-2002
+Prezes Zarz\u0105du
+Silesian Consulting Group sp. z o.o. Brzeg
+
+1999-2002
+Przewodnicz\u0105cy Rady Nadzorczej Sp\xF3\u0142ki
+Uzdrowisko Po\u0142czyn S.A.
+
+1999-2000
+Przewodnic\u0105cy Rady Nadzorczej Sp\xF3\u0142ki
+TBS Wroc\u0142aw sp. z o.o.
+
+1991-2002
+W\u0142a\u015Bciciel
+OP-KOM Oprogramowanie Komputer\xF3w Brzeg
+Doradztwo gospodarcze (tworzenie strategii rozwoju firm i ich wdra\u017Canie).
+Projektowanie i wykonywanie system\xF3w informatycznych wspomagaj\u0105cych zarz\u0105dzanie.
+\u2013 Zaprojektowanie i zaprogramowanie systemu finansowo-ksi\u0119gowego oraz systemu \u015Brodk\xF3w trwa\u0142ych dla \u015Brednich i du\u017Cych firm. 
+\u2013 Stworzenie systemu \u201CMDRS\u201DMission Data Reception System wykorzystywanym przez firm\u0119 EUMETSAT-NIEMCY; przetwarzanie obraz\xF3w, animacja i archiwizacja w bazie
+\u2013 Stworzenie systemu \u201CMDD/SADIS System for PC/SAT\u201D system dla Kuwejtu;czytanie danych satelitarnych, przetwarzanie i tworzenie map pogodowych.
+\u2013 Stworzenie systemu do t\u0142umaczenia  i tworzenia wersji j\u0119zykowych dla CSB-SYSTEM Deutschland.
+\u2013 Wiele innych znacz\u0105cych system\xF3w.
+
+1987-1991
+Prace zdalne
+System modelowania oraz sterowania obrabiarkami numerycznymi dla firmy INTEGRA \u2013 Japonia
+Stworzenie systemu dla gospodarki czasem dla firmy SEV GmbH Niemcy. 
+Du\u017Co znacz\u0105cych prac zleconych w tworzeniu system\xF3w u\u017Cytkowych.
+
+1987-1989
+Projektant \u2013 Programista
+Softsat sp. z o.o.
+AUTOKOMOP sp. z o.o.`,l=[];var w;return i.split(`
+
+`).forEach((k,d)=>{w={date:k.split(`
+`)[0],title:k.split(`
+`)[1],description:k.split(`
+`).splice(2).join(`
+`)},a(1,l=[...l,w])}),[t,l,()=>a(0,t=!t)]}class je extends Q{constructor(e){super();X(this,e,ge,ke,ee,{})}}function ve(m){let e,a,t;return{c(){e=g("link"),a=$(),t=g("div"),this.h()},l(i){const l=ue('[data-svelte="svelte-f6hidj"]',document.head);e=j(l,"LINK",{rel:!0,href:!0}),l.forEach(c),a=S(i),t=j(i,"DIV",{id:!0,class:!0}),h(t).forEach(c),this.h()},h(){n(e,"rel","stylesheet"),n(e,"href","leaflet.css"),n(t,"id","map"),n(t,"class","svelte-1ehnppg")},m(i,l){z(document.head,e),_(i,a,l),_(i,t,l)},p:W,i:W,o:W,d(i){c(e),i&&c(a),i&&c(t)}}}function _e(){let m=L.map("map",{zoomControl:!1,scrollWheelZoom:"center",dragging:!1}).setView([50.85176,17.45952],9),e=L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{maxZoom:18,minZoom:5,attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'});var a=new L.circle([50.8596628,17.4276751],{color:"red",fillColor:"#00aaff",fillOpacity:.5,radius:100}),t=new L.marker([50.85176,17.45952],{opacity:1});return t.bindTooltip("Poprzeczna 15<br>49-300 Brzeg",{permanent:!0,className:"city-name",offset:[55,5],direction:"center"}),e.addTo(m),a.addTo(m),t.addTo(m),m}function $e(m){return me(async()=>{await he(()=>import("../chunks/leaflet-src-9525ae07.js").then(function(e){return e.l}),[]),_e()}),[]}class Se extends Q{constructor(e){super();X(this,e,$e,ve,ee,{})}}function be(m){let e,a,t;return{c(){e=g("span"),a=g("img"),this.h()},l(i){e=j(i,"SPAN",{slot:!0});var l=h(e);a=j(l,"IMG",{class:!0,src:!0,alt:!0}),l.forEach(c),this.h()},h(){n(a,"class","profile-picture svelte-j6elqj"),we(a.src,t="./images/wh.jpg")||n(a,"src",t),n(a,"alt","profile"),n(e,"slot","img")},m(i,l){_(i,e,l),z(e,a)},d(i){i&&c(e)}}}function Ee(m){let e,a;return{c(){e=g("span"),a=b("Wojciech Huczy\u0144ski"),this.h()},l(t){e=j(t,"SPAN",{slot:!0});var i=h(e);a=E(i,"Wojciech Huczy\u0144ski"),i.forEach(c),this.h()},h(){n(e,"slot","title")},m(t,i){_(t,e,i),z(e,a)},d(t){t&&c(e)}}}function Pe(m){let e,a;return{c(){e=g("span"),a=b("Strona ma na celu przybli\u017Cenie dorobku oraz prezentacj\u0119 aktualnych projekt\xF3w"),this.h()},l(t){e=j(t,"SPAN",{slot:!0});var i=h(e);a=E(i,"Strona ma na celu przybli\u017Cenie dorobku oraz prezentacj\u0119 aktualnych projekt\xF3w"),i.forEach(c),this.h()},h(){n(e,"slot","subtitle")},m(t,i){_(t,e,i),z(e,a)},d(t){t&&c(e)}}}function Oe(m){let e,a,t,i,l,w,u,k,d,y,o,r;return{c(){e=g("div"),a=g("p"),t=b("Zdany egzamin pa\u0144stwowy na cz\u0142onk\xF3w Rad Nadzorczych Jednoosobowych Sp\xF3\u0142ek Skarbu Pa\u0144stwa. (1998 r.)"),i=g("br"),l=b(`\r
+			Dyplom Central Connecticut State University New Britain, i Politechniki Wroc\u0142awskiej z uko\u0144czenia podyplomowego Polsko-Ameryka\u0144skiego Studium Komunikacji Spo\u0142ecznej w Organizacji i Zarz\u0105dzaniu.`),w=g("br"),u=b(`\r
+			Certyfikat PRINCE II FOUNDATION metodologia zarz\u0105dzania projektami.`),k=$(),d=g("p"),y=b("Uko\u0144czone kursy i szkolenia w zakresie zarz\u0105dzania opiek\u0105 zdrowotn\u0105, zarz\u0105dzania nieruchomo\u015Bciami samorz\u0105du terytorialnego."),o=g("br"),r=b(`\r
+			Uniwersytet Wroc\u0142awski \u2013 mgr informatyki. Sko\u0144czona specjalno\u015B\u0107 w zakresie informatyki to metody numeryczne. Zdolno\u015B\u0107 realizowania zada\u0144 (samokontrola), umiej\u0119tno\u015B\u0107 planowania i prowadzenia projekt\xF3w oraz umiej\u0119tno\u015Bci systematycznie nabywane w zakresie zarz\u0105dzania, umiej\u0119tno\u015B\u0107 sporz\u0105dzania analizy kosztowej danego przedsi\u0119wzi\u0119cia, umiej\u0119tno\u015B\u0107 prowadzenia negocjacji, oraz wiele innych.`),this.h()},l(f){e=j(f,"DIV",{class:!0});var s=h(e);a=j(s,"P",{style:!0,class:!0});var p=h(a);t=E(p,"Zdany egzamin pa\u0144stwowy na cz\u0142onk\xF3w Rad Nadzorczych Jednoosobowych Sp\xF3\u0142ek Skarbu Pa\u0144stwa. (1998 r.)"),i=j(p,"BR",{}),l=E(p,`\r
+			Dyplom Central Connecticut State University New Britain, i Politechniki Wroc\u0142awskiej z uko\u0144czenia podyplomowego Polsko-Ameryka\u0144skiego Studium Komunikacji Spo\u0142ecznej w Organizacji i Zarz\u0105dzaniu.`),w=j(p,"BR",{}),u=E(p,`\r
+			Certyfikat PRINCE II FOUNDATION metodologia zarz\u0105dzania projektami.`),p.forEach(c),k=S(s),d=j(s,"P",{style:!0,class:!0});var v=h(d);y=E(v,"Uko\u0144czone kursy i szkolenia w zakresie zarz\u0105dzania opiek\u0105 zdrowotn\u0105, zarz\u0105dzania nieruchomo\u015Bciami samorz\u0105du terytorialnego."),o=j(v,"BR",{}),r=E(v,`\r
+			Uniwersytet Wroc\u0142awski \u2013 mgr informatyki. Sko\u0144czona specjalno\u015B\u0107 w zakresie informatyki to metody numeryczne. Zdolno\u015B\u0107 realizowania zada\u0144 (samokontrola), umiej\u0119tno\u015B\u0107 planowania i prowadzenia projekt\xF3w oraz umiej\u0119tno\u015Bci systematycznie nabywane w zakresie zarz\u0105dzania, umiej\u0119tno\u015B\u0107 sporz\u0105dzania analizy kosztowej danego przedsi\u0119wzi\u0119cia, umiej\u0119tno\u015B\u0107 prowadzenia negocjacji, oraz wiele innych.`),v.forEach(c),s.forEach(c),this.h()},h(){le(a,"text-align","justify"),n(a,"class","svelte-j6elqj"),le(d,"text-align","justify"),n(d,"class","svelte-j6elqj"),n(e,"class","rows svelte-j6elqj")},m(f,s){_(f,e,s),z(e,a),z(a,t),z(a,i),z(a,l),z(a,w),z(a,u),z(e,k),z(e,d),z(d,y),z(d,o),z(d,r)},d(f){f&&c(e)}}}function Be(m){let e,a;return e=new je({}),{c(){M(e.$$.fragment)},l(t){K(e.$$.fragment,t)},m(t,i){D(e,t,i),a=!0},i(t){a||(A(e.$$.fragment,t),a=!0)},o(t){I(e.$$.fragment,t),a=!1},d(t){Z(e,t)}}}function Ne(m){let e,a,t,i,l,w,u,k,d,y,o,r,f,s,p,v,O,T,B,H,q,J,F,C,V;return C=new Se({}),{c(){e=g("div"),a=g("div"),t=R("svg"),i=R("path"),l=$(),w=g("a"),u=b("+48 604 907 222"),k=$(),d=R("svg"),y=R("path"),o=$(),r=g("a"),f=b("kontakt@huczynski.pl"),s=$(),p=R("svg"),v=R("path"),O=R("circle"),T=$(),B=g("a"),H=b("Poprzeczna 15, "),q=g("br"),J=b("49-305 Brzeg"),F=$(),M(C.$$.fragment),this.h()},l(N){e=j(N,"DIV",{class:!0});var U=h(e);a=j(U,"DIV",{class:!0});var P=h(a);t=x(P,"svg",{height:!0,viewBox:!0,width:!0,xmlns:!0});var oe=h(t);i=x(oe,"path",{d:!0}),h(i).forEach(c),oe.forEach(c),l=S(P),w=j(P,"A",{href:!0});var re=h(w);u=E(re,"+48 604 907 222"),re.forEach(c),k=S(P),d=x(P,"svg",{height:!0,viewBox:!0,width:!0,xmlns:!0});var ie=h(d);y=x(ie,"path",{d:!0}),h(y).forEach(c),ie.forEach(c),o=S(P),r=j(P,"A",{href:!0});var ne=h(r);f=E(ne,"kontakt@huczynski.pl"),ne.forEach(c),s=S(P),p=x(P,"svg",{height:!0,viewBox:!0,width:!0,xmlns:!0});var Y=h(p);v=x(Y,"path",{d:!0}),h(v).forEach(c),O=x(Y,"circle",{cx:!0,cy:!0,r:!0}),h(O).forEach(c),Y.forEach(c),T=S(P),B=j(P,"A",{href:!0,target:!0,rel:!0});var G=h(B);H=E(G,"Poprzeczna 15, "),q=j(G,"BR",{}),J=E(G,"49-305 Brzeg"),G.forEach(c),P.forEach(c),F=S(U),K(C.$$.fragment,U),U.forEach(c),this.h()},h(){n(i,"d","M5.451 10.29 8.16 7.845c-.462-1.053-.738-2.29-.643-3.9l-3.54-.02c.722 11.37 7.812 15.305 16.019 16.15l.03-3.65c-1.723-.009-2.87-.305-3.966-.627l-2.288 2.691"),n(t,"height","48"),n(t,"viewBox","0 0 24 24"),n(t,"width","48"),n(t,"xmlns","http://www.w3.org/2000/svg"),n(w,"href","tel:+48604907222"),n(y,"d","m2.74 4.826 9.388 7.53 9.131-7.53m-18.518 0h18.518v14.348H2.741Z"),n(d,"height","48"),n(d,"viewBox","0 0 24 24"),n(d,"width","48"),n(d,"xmlns","http://www.w3.org/2000/svg"),n(r,"href","mailto:kontakt@huczynski.pl"),n(v,"d","M19.211 10.382c0 5.094-7.4 10.448-7.4 10.448s-7.022-5.158-7.022-10.448a7.211 7.211 0 1 1 14.422 0z"),n(O,"cx","11.745"),n(O,"cy","10.509"),n(O,"r","1.723"),n(p,"height","48"),n(p,"viewBox","0 0 24 24"),n(p,"width","48"),n(p,"xmlns","http://www.w3.org/2000/svg"),n(B,"href","https://goo.gl/maps/CxLpJpsxGxfNQfKD8"),n(B,"target","_blank"),n(B,"rel","noopener noreferrer"),n(a,"class","contact-info svelte-j6elqj"),n(e,"class","map-grid svelte-j6elqj")},m(N,U){_(N,e,U),z(e,a),z(a,t),z(t,i),z(a,l),z(a,w),z(w,u),z(a,k),z(a,d),z(d,y),z(a,o),z(a,r),z(r,f),z(a,s),z(a,p),z(p,v),z(p,O),z(a,T),z(a,B),z(B,H),z(B,q),z(B,J),z(e,F),D(C,e,null),V=!0},i(N){V||(A(C.$$.fragment,N),V=!0)},o(N){I(C.$$.fragment,N),V=!1},d(N){N&&c(e),Z(C)}}}function Re(m){let e,a,t,i,l,w,u,k,d,y;return e=new fe({}),t=new ye({props:{$$slots:{subtitle:[Pe],title:[Ee],img:[be]},$$scope:{ctx:m}}}),l=new ae({props:{title:"O mnie",first:!0,$$slots:{default:[Oe]},$$scope:{ctx:m}}}),u=new ae({props:{title:"Curriculum Vitae",id:"CV",$$slots:{default:[Be]},$$scope:{ctx:m}}}),d=new ae({props:{title:"Kontakt",first:!0,background:"var(--gradient)",$$slots:{default:[Ne]},$$scope:{ctx:m}}}),{c(){M(e.$$.fragment),a=$(),M(t.$$.fragment),i=$(),M(l.$$.fragment),w=$(),M(u.$$.fragment),k=$(),M(d.$$.fragment)},l(o){K(e.$$.fragment,o),a=S(o),K(t.$$.fragment,o),i=S(o),K(l.$$.fragment,o),w=S(o),K(u.$$.fragment,o),k=S(o),K(d.$$.fragment,o)},m(o,r){D(e,o,r),_(o,a,r),D(t,o,r),_(o,i,r),D(l,o,r),_(o,w,r),D(u,o,r),_(o,k,r),D(d,o,r),y=!0},p(o,[r]){const f={};r&1&&(f.$$scope={dirty:r,ctx:o}),t.$set(f);const s={};r&1&&(s.$$scope={dirty:r,ctx:o}),l.$set(s);const p={};r&1&&(p.$$scope={dirty:r,ctx:o}),u.$set(p);const v={};r&1&&(v.$$scope={dirty:r,ctx:o}),d.$set(v)},i(o){y||(A(e.$$.fragment,o),A(t.$$.fragment,o),A(l.$$.fragment,o),A(u.$$.fragment,o),A(d.$$.fragment,o),y=!0)},o(o){I(e.$$.fragment,o),I(t.$$.fragment,o),I(l.$$.fragment,o),I(u.$$.fragment,o),I(d.$$.fragment,o),y=!1},d(o){Z(e,o),o&&c(a),Z(t,o),o&&c(i),Z(l,o),o&&c(w),Z(u,o),o&&c(k),Z(d,o)}}}class De extends Q{constructor(e){super();X(this,e,null,Re,ee,{})}}export{De as default};
